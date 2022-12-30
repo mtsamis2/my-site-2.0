e98288cfa61206121457b27fb2294264148f0209
mtsamis2 <mtsamis@gmail.com>
2022-12-30 12:33:55 -0500

    Categories pages implemented

---
+++ pages/categories/[category].js
@@ -0,0 +1,77 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import ErrorPage from 'next/error'
+import PostBody from '../../components/post-body'
+import MoreStories from '../../components/more-stories'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import Layout from '../../components/layout'
+import { getAllCategories, getPostsByCategory, getCategoryBySlug } from '../../lib/api'
+import PostTitle from '../../components/post-title'
+import { CMS_NAME } from '../../lib/constants'
+import DateComponent from '../../components/date'
+
+export default function Category({ posts, category, preview }) {
+  const router = useRouter()
+
+  if (!router.isFallback && !category) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <div className='single'>
+     <div id='wrapper'>
+      <Layout preview={preview}>
+        {router.isFallback ?
+          (<PostTitle>Loading…</PostTitle>) :
+          (
+            <>
+              <header>
+                  <h3>Posts with the category "{category.title}"</h3>
+              </header>
+              <div className="categories">
+                {posts.map((post, key) => (
+                  <article className="mini-post" key={key}>
+                    <header>
+                      <h3>
+                          <a href={`/posts/${post.slug}`} >{post.title}</a>
+                      </h3>
+                      <time className="published">
+                        <DateComponent dateString={post.date}/>
+                      </time>
+                    </header>
+                    <a className="image" href={`/posts/${post.slug}`}>
+                      <img src={post.coverImage.url}/>
+                    </a>
+                  </article>
+                ))}
+              </div>
+            </>
+          )
+        }
+      </Layout>
+     </div>
+    </div>
+  )
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const category = await getCategoryBySlug(preview, params.category)
+  const posts = category ? await getPostsByCategory(preview, category.slug) : null
+
+  return {
+    props: {
+      posts,
+      category,
+      preview
+    } 
+  }
+}
+
+export async function getStaticPaths() {
+  const allCategories = await getAllCategories()
+  return {
+    paths: allCategories?.map(({ slug }) => `/categories/${slug}`) ?? [],
+    fallback: false
+  }
+}
