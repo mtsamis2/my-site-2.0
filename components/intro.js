d78edf3db7254817891c96b38fcbbcaa4fdca60b
mtsamis2 <mtsamis@gmail.com>
2023-01-09 14:22:54 -0500

    dont lazy load intro

--- components/intro.js
+++ components/intro.js
@@ -1,6 +1,6 @@
 const Intro = (props) => (
   <section id="intro" className={props.className}>
-      <div className="logo"><img src="/images/logo.png" alt="MikeTsamis.com Logo" loading='lazy' /></div>
+      <div className="logo"><img src="/images/logo.png" alt="MikeTsamis.com Logo" /></div>
           <header>
               <h2>Mike<br/>Tsamis</h2>
               <p>IDEATE. CREATE. ITERATE. REPEAT.</p>
